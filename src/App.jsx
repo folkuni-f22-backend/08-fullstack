ccd8de67bb7acfc8e1285d4d30beff19e2eda65e
David Andersson <david.andersson@zocom.se>
2023-05-31 10:59:53 +0200

    Complete fullstack app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [cities, setCities] = useState(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleClick = async () => {
+		// TODO: använd try/catch. Visa meddelande till användaren om något går fel.
+		const response = await fetch('/api/cities')
+		const data = await response.json()
+		setCities(data)
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick}> Hämta städer! </button>
+			{cities
+				? cities.map(({ id, name }) => (
+					<div key={id}> {name} </div>
+				))
+				: <p> Klicka för att hämta städer från API:et </p>
+			}
+		</div>
+	)
 }
 
 export default App
