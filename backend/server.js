febac4683764b2c40d8af76226d2a52282b12e50
David Andersson <david.andersson@zocom.se>
2023-05-31 11:54:12 +0200

    Servar dist-mappen med express.static

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import citiesRouter from './routes/cities.js'
 import searchRouter from './routes/search-cities.js'
 
@@ -11,6 +13,11 @@ app.use((req, res, next) => {
 	console.log(`${req.method}  ${req.url}`, req.body);
 	next()
 })
+
+
+const whereWeAre = dirname(fileURLToPath(import.meta.url))
+const dist = join(whereWeAre, '../dist')
+app.use( express.static(dist) )
 
 //   /api/cities
 app.use('/api/cities', citiesRouter)
